5a60df59737e7e57453500af9c664ea1c7dd21c2
Drishti Aggarwal <97733511+DrishtiiAggarwal@users.noreply.github.com>
2024-04-15 00:47:55 +0530

    clerk auth

---
+++ app/(auth)/sign-up/[[...sign-up]]/page.tsx
@@ -0,0 +1,11 @@
+import { SignUp } from '@clerk/nextjs'
+import React from 'react'
+
+const SignUpPage = () => {
+  console.log("hello");
+  return (
+    <SignUp/>
+  )
+}
+
+export default SignUpPage
