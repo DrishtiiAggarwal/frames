7df8388e4f4c67a5a325c37ebe0f07cc744917be
Drishti Aggarwal <97733511+DrishtiiAggarwal@users.noreply.github.com>
2024-04-18 01:06:48 +0530

    code

--- public/middleware.ts
+++ public/middleware.ts
@@ -3,5 +3,8 @@ import { authMiddleware } from "@clerk/nextjs";
 export default authMiddleware({});
 
 export const config = {
-  matcher: ["/((?!.+.[w]+$|_next).*)", "/", "/(api|trpc)(.*)"],
+  matcher: ["/((?!.+.[w]+$|_next).*)", "/", "/(api|trpc)(.*)",
+  '/((?!api|_next/static|_next/image|favicon.ico).*)'
+],
+
 };
